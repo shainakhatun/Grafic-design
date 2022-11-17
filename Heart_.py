182d949873c9cc07e8e42f3e78424d3d46c033a5
shainakhatun <116898401+shainakhatun@users.noreply.github.com>
2022-11-17 14:46:34 +0530

    Add files via upload

---
+++ Heart_.py
@@ -0,0 +1,17 @@
+# (i) Heart in red colour....
+
+import turtle as t
+t.pensize(5)
+t.speed(1)
+t.color('black')
+t.begin_fill()
+t.fillcolor('red')
+t.left(150)
+t. forward (180)
+t.circle(-90,180)
+t.setheading (60)
+t.circle(-90,180)
+t. forward (180)
+t.end_fill()
+t.hideturtle()
+t.done()
